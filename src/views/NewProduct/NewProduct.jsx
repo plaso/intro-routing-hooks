795a89d5bab400bd6abab7057978b345b5b4d8c2
Pablo Laso Cortabitarte <plasocortabitarte@gmail.com>
2023-03-18 14:34:02 +0100

    feat: errors

--- src/views/NewProduct/NewProduct.jsx
+++ src/views/NewProduct/NewProduct.jsx
@@ -6,11 +6,41 @@ import Select from "../../components/misc/Select/Select"
 import { getManufacturers } from "../../services/ManufacturersService"
 import { createProduct } from "../../services/ProductsService"
 
+const REQUIRED_MSG = 'Complete this field'
+const URL_REGEX = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/
+
+
 const INITIAL_VALUES = {
   name: '',
   price: 0,
   manufacturerId: '',
   img: ''
+}
+
+const formSchema = {
+  name: (value) => {
+    if (!value) {
+      return REQUIRED_MSG
+    }
+  },
+  price: (value) => {
+    if (value === undefined || value === null) {
+      return REQUIRED_MSG
+    }
+    if (value <= 0) {
+      return 'Price must be greater than 0'
+    }
+  },
+  manufacturerId: (value) => {
+    if (!value) {
+      return REQUIRED_MSG
+    }
+  },
+  img: (value) => {
+    if (!URL_REGEX.test(value)) {
+      return 'Enter a valid URL'
+    }
+  }
 }
 
 const NewProduct = () => {
@@ -29,11 +59,36 @@ const NewProduct = () => {
 
   const onSubmit = (e) => {
     e.preventDefault()
+    // ['name', 'img']
 
-    createProduct(values)
-      .then((createdProduct) => {
-        navigate(`/products/${createdProduct.id}`)
-      })
+    const obj = { hola: 'pepe' }
+
+    const newErrorsObj = {}
+
+    /*
+    newErrorsObj = {
+      name: 'Complete this field' Si hay errores esto es lo que iriamos montando
+    }*/
+
+    Object.keys(formSchema).forEach(key => {
+      const validator = formSchema[key]
+      const value = values[key]
+
+      const error = validator(value)
+
+      if (error) {
+        newErrorsObj[key] = error
+      }
+    })
+
+    if (Object.keys(newErrorsObj).length === 0) {
+      createProduct(values)
+        .then((createdProduct) => {
+          navigate(`/products/${createdProduct.id}`)
+        })
+    } else {
+      setErrors(newErrorsObj)
+    }
   }
 
   const onChange = (event) => {
@@ -57,30 +112,30 @@ const NewProduct = () => {
       <h1>New Product</h1>
 
       <form onSubmit={onSubmit}>
-        <FormControl htmlFor="name" text="Name">
+        <FormControl htmlFor="name" text="Name" error={errors.name}>
           <Input
             id="name" name="name" placeholder="Name of the new product"
-            value={values.name} onChange={onChange}
+            value={values.name} onChange={onChange} error={errors.name}
           />
         </FormControl>
 
-        <FormControl htmlFor="price" text="Price">
+        <FormControl htmlFor="price" text="Price" error={errors.price}>
           <Input
-            id="price" name="price"
+            id="price" name="price" error={errors.price}
             value={values.price} onChange={onChange} type="number" min={0}
           />
         </FormControl>
 
-        <FormControl htmlFor="manufacturer" text="Manufacturer">
+        <FormControl htmlFor="manufacturer" text="Manufacturer" error={errors.manufacturerId}>
           <Select
             id="manufacturer" name="manufacturerId" value={values.manufacturerId}
-            options={mappedOptions} onChange={onChange}
+            options={mappedOptions} onChange={onChange} error={errors.manufacturerId}
           />
         </FormControl>
 
-        <FormControl htmlFor="img" text="Image">
+        <FormControl htmlFor="img" text="Image" error={errors.img}>
           <Input
-            id="img" name="img"
+            id="img" name="img" error={errors.img}
             value={values.img} onChange={onChange} type="url"
           />
         </FormControl>
